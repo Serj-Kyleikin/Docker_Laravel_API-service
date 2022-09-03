e749f684e4f0b26c48a15b2d5ae07f5db6d53d66
Serj <93035922+Serj-Kyleikin@users.noreply.github.com>
2022-09-03 21:18:47 +0300

    Add files via upload

--- project/app/Services/Service.php
+++ project/app/Services/Service.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Country;
 use App\Models\City;
 use App\Models\CountryCity;
@@ -9,113 +10,167 @@ use App\Models\CountryCity;
 class Service {
 
     public $country;
-    public $status = false;
 
-    public function addCountry($data) {
+    public $messages = [
+        'The service is not available',
+        'The country already exists!',
+        'The city already exists!'
+    ];
 
-        $country = Country::firstOrCreate(
-            ['title' =>  $data['title']]
-        );
+    public function addCountry($data) 
+    {
+        DB::beginTransaction();
 
-        if($country->wasRecentlyCreated) {
+        try {
 
-            if(isset($data['city']) and $data['city']) {
+            $country = Country::firstOrCreate(
+                ['title' =>  $data['title']]
+            );
 
-                $city = City::firstOrCreate(
-                    ['title' =>  $data['city']]
-                );
+            if($country->wasRecentlyCreated) {
 
-                if($city->wasRecentlyCreated) {
+                if(isset($data['city']) and $data['city']) {
+
+                    $city = City::firstOrCreate(
+                        ['title' =>  $data['city']]
+                    );
+
+                    if($city->wasRecentlyCreated) {
+
+                        $add['country_id'] = $country->id;
+                        $add['city_id'] = $city->id;
+
+                        CountryCity::create($add);
+
+                    } else {
+
+                        CountryCity::updateOrInsert(
+                            ['city_id' =>  $city->id],
+                            ['country_id' => $country->id]
+                        );
+                    }
+                }
+
+                $response = ['id' => $country->id, 'code' => 201];
+
+            } else $response = ['message' => $this->messages[1], 'code' => 422];
+
+            DB::commit();
+            return $response;
+
+        } catch(\Exception $exception) {
+
+            DB::rollback();
+            return ['message' => $this->messages[0], 'code' => 503];
+        }
+    }
+
+    public function updateCountry($entity, $data) 
+    {
+        DB::beginTransaction();
+
+        try {
+
+            $status = Country::where('title', $data['title'])->firstOr(function () use ($entity, $data) {
+                $entity->update($data);
+            });
+
+            DB::commit();
+            return (isset($status->wasRecentlyCreated)) ? ['message' => $this->messages[1], 'code' => 422] : ['code' =>  204];
+
+        } catch(\Exception $exception) {
+
+            DB::rollback();
+            return ['message' => $this->messages[0], 'code' => 503];
+        }
+    }
+
+    public function addCity($data) 
+    {
+        DB::beginTransaction();
+
+        try {
+        
+            $city = City::firstOrCreate(
+                ['title' =>  $data['title']]
+            );
+
+            if($city->wasRecentlyCreated) {
+                
+                if(isset($data['country']) and $data['country']) {
+
+                    $country = Country::firstOrCreate(
+                        ['title' =>  $data['country']]
+                    );
 
                     $add['country_id'] = $country->id;
                     $add['city_id'] = $city->id;
 
                     CountryCity::create($add);
-
-                } else {
-
-                    CountryCity::updateOrInsert(
-                        ['city_id' =>  $city->id],
-                        ['country_id' => $country->id]
-                    );
                 }
-            }
 
-            return $country;
+                $response = ['id' => $city->id, 'code' => 201];
 
-        } else return 0;
+            } else $response = ['message' => $this->messages[2], 'code' => 422];
+
+            DB::commit();
+            return $response;
+
+        } catch(\Exception $exception) {
+
+            DB::rollback();
+            return ['message' => $this->messages[0], 'code' => 503];
+        }
     }
 
-    public function updateCountry($entity, $data) {
+    public function updateCity($entity, $data) 
+    {
+        DB::beginTransaction();
 
-        Country::where('title', $data['title'])->firstOr(function () use ($entity, $data) {
-            $entity->update($data);
-            $this->status = true;
-        });
+        try {
 
-        return $this->status;
-    }
+            City::where('title', $data['title'])->firstOr(function () use ($entity, $data) {
 
-    public function addCity($data) {
-        
-        $city = City::firstOrCreate(
-            ['title' =>  $data['title']]
-        );
-
-        if($city->wasRecentlyCreated) {
-            
-            if(isset($data['country']) and $data['country']) {
-
-                $country = Country::firstOrCreate(
-                    ['title' =>  $data['country']]
-                );
-
-                $add['country_id'] = $country->id;
-                $add['city_id'] = $city->id;
-
-                CountryCity::create($add);
-            }
-
-            return 1;
-
-        } else return 0;
-    }
-
-    public function updateCity($entity, $data) {
-
-        $city = City::where('title', $data['title'])->firstOr(function () use ($entity, $data) {
-
-            $insert['title'] = $data['title'];
-            $entity->update($insert);
-        });
-
-        // Приоритет у новой созданной страны
-
-        if(isset($data['country']) and $data['country']) {
-
-            $country = Country::where('title', $data['country'])->firstOr(function () use ($data) {
-
-                $insert['title'] = $data['country'];
-                $this->country = Country::create($insert);
+                $insert['title'] = $data['title'];
+                $entity->update($insert);
             });
 
-            $id = ($country) ? $country->id : $this->country->id;
+            // Приоритет у новой созданной страны
 
-            CountryCity::updateOrInsert(
-                ['city_id' => $entity->id],
-                ['country_id' => $id]
-            );
+            if(isset($data['country']) and $data['country']) {
 
-        } else {
+                $country = Country::where('title', $data['country'])->firstOr(function () use ($data) {
 
-            if(isset($data['country_id']) and $data['country_id']) {
+                    $insert['title'] = $data['country'];
+                    $this->country = Country::create($insert);
+                });
+
+                $id = ($country) ? $country->id : $this->country->id;
 
                 CountryCity::updateOrInsert(
                     ['city_id' => $entity->id],
-                    ['country_id' => $data['country_id']]
+                    ['country_id' => $id]
                 );
 
-            } else CountryCity::where('city_id', $entity->id)->delete();
+            } else {
+
+                if(isset($data['country_id']) and $data['country_id']) {
+
+                    CountryCity::updateOrInsert(
+                        ['city_id' => $entity->id],
+                        ['country_id' => $data['country_id']]
+                    );
+
+                } else CountryCity::where('city_id', $entity->id)->delete();
+            }
+
+            DB::commit();
+            return ['code' => 204];
+
+        } catch(\Exception $exception) {
+
+            DB::rollback();
+            return ['message' => $this->messages[0], 'code' => 503];
         }
     }
 }
